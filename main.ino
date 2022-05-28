185211fb70e2672b2e54bbe13d5e1ada3039facc
FerdinandoPH <perez.holguin@gmail.com>
2022-05-28 22:08:50 +0200

    vel ajustable

--- main.ino
+++ main.ino
@@ -1,7 +1,21 @@
 #define MOTOR 11
+int vel=95;
+
 void setup(){
     pinMode(MOTOR, OUTPUT);
+    Serial.begin(9600);
+    Serial.println("HOLA");
 }
 void loop(){
-    analogWrite(MOTOR, 95);
+    if (Serial.available()>0){
+        vel=Serial.readString().toInt();
+        if (vel>255){
+            vel=255;
+        }
+        else if (vel <-255){
+            vel=-255;
+        }
+        
+    }
+    analogWrite(MOTOR, vel);
 }
